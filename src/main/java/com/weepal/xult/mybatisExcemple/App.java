261fbf2c070d04f87cd164c8ac97d21b2a27d24b
LT <991073657@qq.com>
2017-03-15 11:24:28 +0800

    新建项目
    将LT学习mybatis项目在GitHub托管

---
+++ src/main/java/com/weepal/xult/mybatisExcemple/App.java
@@ -0,0 +1,13 @@
+package com.weepal.xult.mybatisExcemple;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
